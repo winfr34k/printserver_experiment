e8d86a08f97e06195b696cbc9f4be8d74949bd42
Thomas Keppler <winfr34k@gmail.com>
2017-10-03 00:11:53 +0200

    Fix typo

--- 00-initialize-ansible-master.sh
+++ 00-initialize-ansible-master.sh
@@ -57,7 +57,7 @@ apt-key adv --keyserver "${URL_KEYSERVER}" --recv-keys "${KEY_ANSIBLE}"
 # Install Ansible
 apt -y update
 apt -y dist-upgrade
-apt -y install git python ansible openssh-server rsync python-dnspython ntfs-3g cowsay python-expect
+apt -y install git python ansible openssh-server rsync python-dnspython ntfs-3g cowsay python-pexpect
 echo "Done."
 echo
 
